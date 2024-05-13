30ec9d70c03eb470003240248b779f40aa762016
ikhlasdansantai <initesting5@gmail.com>
2024-05-13 09:35:04 +0700

    aplikasi gak akan sah, kalo gak dikasih hello world

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "@/components/Themed";
 export default function TabOneScreen() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-4xl text-red-600">Tab One</Text>
+      <Text className="text-4xl ">Hello World 😎🚀</Text>
       <View lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
